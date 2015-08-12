afdfbb91759aa0ec1e22585e9966019c143ac077
perlancar (@pc-office) <perlancar@gmail.com>
2015-08-12 20:18:24 +0700

    Provide separate ::NonWideUtil and ::WideUtil

---
+++ lib/Text/ANSI/NonWideUtil.pm
@@ -0,0 +1,34 @@
+package Text::ANSI::NonWideUtil;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict 'subs', 'vars';
+use warnings;
+
+require Exporter;
+our @ISA       = qw(Exporter);
+our @EXPORT_OK = qw(
+                       ta_add_color_resets
+                       ta_detect
+                       ta_extract_codes
+                       ta_highlight
+                       ta_highlight_all
+                       ta_length
+                       ta_length_height
+                       ta_pad
+                       ta_split_codes
+                       ta_split_codes_single
+                       ta_strip
+                       ta_trunc
+                       ta_wrap
+               );
+
+use Text::ANSI::BaseUtil;
+
+our $re = $Text::ANSI::BaseUtil::re;
+*{$_} = \&{"Text::ANSI::BaseUtil::$_"} for @EXPORT_OK;
+
+1;
+# ABSTRACT: Routines for text containing ANSI escape codes (non-wide functions only)
---
+++ lib/Text/ANSI/WideUtil.pm
@@ -0,0 +1,28 @@
+package Text::ANSI::WideUtil;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict 'subs', 'vars';
+use warnings;
+
+use Text::WideChar::Util qw(mbswidth mbtrunc);
+
+require Exporter;
+our @ISA       = qw(Exporter);
+our @EXPORT_OK = qw(
+                       ta_mbpad
+                       ta_mbswidth
+                       ta_mbswidth_height
+                       ta_mbtrunc
+                       ta_mbwrap
+               );
+
+use Text::ANSI::BaseUtil;
+
+our $re = $Text::ANSI::BaseUtil::re;
+*{$_} = \&{"Text::ANSI::BaseUtil::$_"} for @EXPORT_OK;
+
+1;
+# ABSTRACT: Routines for text containing ANSI escape codes (wide functions only)
